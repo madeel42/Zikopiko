c43c6eab26541a775826424ebed7befd72b2a4fb
madeel <madeelakhtar204@gmail.com>
2022-01-31 15:58:53 +0500

    update with new canges

--- src/components/home/Testimonial.js
+++ src/components/home/Testimonial.js
@@ -1,8 +1,9 @@
 import vid from '../../assests/nft.mp4'
+import Pdf from '../../assests/pdf.jpg'
 const Testimonial = () => {
     return (
         <div className="testimonial-section-wrapper">
-            <div className="row">
+            {/* <div className="row">
                 <div className="col-sm-12 col-md-6 col-lg-4 order-md-last">
                     <div className="testimonial-left-section">
                         <img src="assets/images/download (1).png" alt="" />
@@ -17,12 +18,19 @@ const Testimonial = () => {
                             He throws jets of liquid at the viewer. As some of the paint land on canvas, he begins to dialogue with you, but does not seem to be talking to you, as he is yelling at you. His work provides food for thought though some of the offering might look toxic, and taste acidic”.</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
             <div className="row">
                 <div className="col-sm-12 col-md-12 col-lg-12">
                     <div className="testimonial-left-section">
                         <video width='100%' height='300' src={vid} controls />
                     </div>
+                </div>
+            </div>
+            <div className="row" style={{overflow:'auto'}}>
+                <div className="col-sm-12 col-md-12 col-lg-12" >
+                    {/* <div className="testimonial-left-section"> */}
+                        <img src={Pdf} alt="" />
+                    {/* </div> */}
                 </div>
             </div>
         </div>
